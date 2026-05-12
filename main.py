5a3f13d24df75e5ae379d87ba2a21ab5da9677d3
Trizzen <terrellpotts@gmail.com>
2026-05-12 13:29:40 -0400

    chore: initialize agentic AI course project

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    if len(sys.argv) < 2:
+        print("I need you to give me a prompt right now")
+        sys.exit(1)
+    verbose_flag = False
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        verbose_flag = True
+    prompt = sys.argv[1]
+
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
+
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', contents=messages
+    )
+
+
+
+    print(response.text)
+    if response is None or response.usage_metadata is None:
+        print("No usage metadata looking right")
+        return
+
+    if verbose_flag:
+            print (f"User Prompt: {prompt}")
+            print (f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print (f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+main()
