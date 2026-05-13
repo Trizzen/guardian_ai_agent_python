0f5f9c9a4aee86f605a7cad64d257c2f81db0500
Trizzen <terrellpotts@gmail.com>
2026-05-13 18:07:53 -0400

    Commit before get_file_content.py

---
+++ functions/get_file_content.py
@@ -0,0 +1,29 @@
+import os
+from config import MAX_CHARS
+
+def get_file_content(working_directory, file_path):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: *{file_path}* is not in the working directory'
+    if not os.path.isfile(abs_file_path):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+    file_content_string = ""
+    try:
+        with open(abs_file_path, 'r') as f:
+            file_content_string = f.read(MAX_CHARS)
+            if len(file_content_string) >= MAX_CHARS:
+                file_content_string += (
+                    f'[...File "{file_path}" truncated at 10000 characters]'
+                )
+        return file_content_string
+    except Exception as e:
+        return f"Exception reading file: {e}"
+
+    # if directory is None:
+    #     # directory = working_directory
+    #     abs_directory = os.path.abspath(working_directory)
+    # else:
+    #     abs_directory = os.path.abspath(os.path.join(working_directory, directory))
